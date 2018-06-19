fef2fba606510ac8c974756755ac300382605fc3
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-19 19:42:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,21 @@
 def fizz_buzz(int)
   if int %3 == 0
     puts "Fizz"
-  end
+
+
+  elsif int %5 == 0
+    puts "Buzz"
+
+
+  elsif int %15 == 0
+    puts "FizzBuzz"
+
+  else
+    puts "nil"
+
+  end     
 end
 
 fizz_buzz(3)
+fizz_buzz(5)
+fizz_buzz(15)
